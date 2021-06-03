31e1f42eb4b285d336bd559b93549c5cf6ca28c2
enzo devoto <enzodevotob@gmail.com>
2021-06-03 18:05:26 -0400

    probando un extra

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = () => {
+let addCarts = () => {
   document.querySelector(".card").classList.add(randomType());
   document.querySelector(".card").innerHTML = randomNumber();
+
+
+//Con el boton de draw y el text input generar la cantidad de cartas que es ingresada.
+//Con el boton sort mostrar las cartas en orden, usando el algoritmo bubble.
+//Guardar los cambios(tirada de cartas) en un array.
+//mostrar por console.log las 'manos' que se imprimieron.
+
+
+
+
+
 };
+
+
+
+
+
+
+
+
 
 let randomNumber = () => {
   let numbers = [
