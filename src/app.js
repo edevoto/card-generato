a29826e62c10750072a12f5bd989d5b19ff7173a
enzo devoto <enzodevotob@gmail.com>
2021-06-03 17:11:00 -0400

    card generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(randomType());
+  document.querySelector(".card").innerHTML = randomNumber();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let numI = Math.floor(Math.random() * numbers.length);
+
+  return numbers[numI];
+};
+
+let randomType = () => {
+  let type = ["diamond", "spades", "heart", "clover"];
+  let typeI = Math.floor(Math.random() * type.length);
+
+  return type[typeI];
 };
