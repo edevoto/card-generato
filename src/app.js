91e1f9383e1aa895190bc1fd98d58422dbfdd978
enzo devoto <enzodevotob@gmail.com>
2021-06-10 16:48:03 -0400

    Avances

--- src/app.js
+++ src/app.js
@@ -34,14 +34,12 @@ let randomType = () => {
 //Need to place the cardas one on side of another. with clasees.
 
 let cardArray = [];
-
+let cardContainer = document.querySelector(".card-container");
 const cardGenerator = () => {
-  let cardContainer = document.querySelector(".card-container");
   let inputValue = document.querySelector("#numberOfCards").value; // Creamos esto para obtener el valor del input.
 
   for (let i = 0; i < inputValue; i++) {
     let type = randomType();
-
     let div = document.createElement("DIV");
     div.classList.add(type);
     let text = document.createElement("H1");
@@ -65,7 +63,7 @@ const cardGenerator = () => {
 };
 
 //Hacer un recorrido al cardArray y con el mismo metodo para mostar las cartas mostrar ya obtenidas. pero sin el random si no desde el array.
-let printSortedArray = [];
+
 const sortedArray = arr => {
   let wall = arr.length - 1; //we start the wall at the end of the array
   while (wall > 0) {
@@ -82,17 +80,35 @@ const sortedArray = arr => {
     wall--; //decrease the wall for optimization
   }
 
-  arr.forEach(card => {
-    printSortedArray.push(card);
+  let newCardContainer = document.createElement("DIV");
+  let card = document.querySelector(".card");
+  cardArray.forEach(card => {
+    //const pinta =
+    const valor = cardArray.tipo;
+
+    document.createElement("DIV");
+    let newDiv = document.querySelector(".card");
+    newDiv.classList.add(
+      "card",
+      "justify-content-between",
+      "text-center",
+      card.numero
+    );
+    console.log(newDiv);
+    let text = document.createElement("H1");
+    text.textContent = valor;
+    newCardContainer.appendChild(newDiv);
+    console.log(card);
   });
-  return arr;
+  card.parentNode.remove(card);
+  return newCardContainer;
 };
 
 let drawButton = document.querySelector("#draw");
 drawButton.addEventListener("click", cardGenerator);
+
 let sortButton = document.querySelector("#sort");
+
 sortButton.addEventListener("click", function() {
   sortedArray(cardArray);
-
-  console.log(printSortedArray);
 });
